916d08c820ceee01c8046788dd86325f2d8bdeff
Cyrto <1461638083@qq.com>
2022-11-11 15:17:01 +0800

    fix: remove useless utils

--- src/utils.ts
+++ src/utils.ts
@@ -30,21 +30,6 @@ export function BnDiv(a: Numberish, b: Numberish): BigNumber {
 }
 
 
-
-export function hexFeltToString(hexFelt: string): string {
-  let felt: bigint = BigInt(hexFelt);
-  const newStrB = Buffer.from(felt.toString(16), "hex");
-  return newStrB.toString();
-}
-
-export const hexToDecimalNumber = (hex) => Number(hexToDecimal(hex));
-
-export const hexToDecimal = (hex) => BigInt(hex).toString(10);
-
-export function stringToFelt(str: any) {
-  return "0x" + Buffer.from(str).toString("hex");
-}
-
 // big number
 export const ten = new BigNumber(10);
 
@@ -52,18 +37,4 @@ export function newBnWithDecimals(base: number | string | BigNumber, decimals: n
   let _decimals = new BigNumber(decimals);
   let _base = new BigNumber(base);
   return _base.multipliedBy(ten.pow(_decimals));
-}
-
-export const toMessageTimestamp = (date: Date | number): number => {
-  return Number((new Date(date).getTime() / 10).toFixed(0));
-};
-
-export function unwrapDecimals(base: number | string | BigNumber, decimals: number, places?: number): BigNumber {
-  let _decimals = new BigNumber(decimals);
-  let _base = new BigNumber(base);
-  let ret = _base.div(ten.pow(_decimals));
-  if (places > 0) {
-    ret = new BigNumber(ret.toFixed(places, BigNumber.ROUND_DOWN));
-  }
-  return ret;
 }
